790e8a3ff1699b2ce1c280fe10d5878146eb9b7d
Tanya Hlopenkova <tanyahlopenkova@gmail.com>
2021-11-04 18:43:52 +0300

    make burger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* Header Script */
 
+// При прокрутке меняет цвет header
 const header = document.querySelector('.header');
 const slideHeight = document.querySelector('.slide').clientHeight;
 
@@ -8,3 +9,37 @@ window.addEventListener('scroll', function () {
     header.style.backgroundColor = scrollFromTop > slideHeight ? '#EBEBEB' : '';
     }
 )
+
+// Burger
+document.getElementById('trigger').onclick = function () {
+    open()
+};
+
+function open() {
+    document.getElementById('menu').classList.toggle('active');
+    document.body.classList.toggle('lock')
+    document.getElementById('trigger').classList.toggle('header__close');
+}
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.list__item');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+
+            window.scroll({
+                top: gotoBlockValue,
+                behavior: "smooth",
+            });
+            e.preventDefault();
+        }
+    }
+}
